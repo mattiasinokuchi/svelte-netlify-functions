bac6abd9df1050fc2ed07724e3b2e91479ff916c
mattiasinokuchi <8-b@tuta.io>
2021-03-22 18:29:26 +0100

    changed to export deafault

--- api/place.js
+++ api/place.js
@@ -1,6 +1,4 @@
-// API usage endpoint
-app.get("/api/whoami", (req, res) => {
-    res.json({ipaddress: req.headers["x-forwarded-for"].split(",")[0], language: req.headers["accept-language"], software: req.headers["user-agent"]});
-    console.log(req.headers["x-forwarded-for"].split(",")[0]);
-  });
-  
+export default (req, res) => {
+  res.json({ ipaddress: req.headers["x-forwarded-for"].split(",")[0], language: req.headers["accept-language"], software: req.headers["user-agent"] });
+  console.log(req.headers["x-forwarded-for"].split(",")[0]);
+}
