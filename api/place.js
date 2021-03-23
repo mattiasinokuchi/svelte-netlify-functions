0ab1825f7575d6c29bdcd3f89ffe11a09b859f55
mattiasinokuchi <8-b@tuta.io>
2021-03-23 04:28:38 +0100

    changed function format

--- api/place.js
+++ api/place.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-export default async (req, res) => {
+export default async function (req, res) {
   let response = await fetch("http://ip-api.com/json/");
   let place = await response.json();
   res.json(place);
